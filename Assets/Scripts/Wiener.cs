a28a51914c21a309e9cf4e3673c3c2f3d6f17da3
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2024-04-04 15:12:33 +0200

    Started noise function (doesn't compile) #10

---
+++ Assets/Scripts/Wiener.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Wiener : MonoBehaviour
+{
+    
+   public double GenerateNoise
+        (double initPos, double drift, double diffusion, double deltaTime, int numSteps)
+    {
+        double[] wienerProcess = new double[numSteps];
+        double currentPos = initPos;
+
+        for (int i = 0; i < numSteps; i++)
+        {
+            double randomValue = Random.Range(0f, 1.0f);
+            double increment = drift * deltaTime + Mathf.Sqrt((float)deltaTime) * diffusion * randomValue;
+            currentPos += increment;
+            wienerProcess[i] = currentPos;
+        }
+
+        return wienerProcess;
+    }
+}
