b5569e2ef44f1777f82d509739238dcac5b4f963
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2024-04-10 15:11:05 +0200

    Started work on character class and JSON reader #20

--- Assets/Scripts/Clients/ClientTemplate.cs
+++ Assets/Scripts/Clients/ClientTemplate.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Serialization;
 using UnityEngine;
 
-public class ClientTemplaet : MonoBehaviour
+public class ClientTemplate : MonoBehaviour
 {
+    public ClientData clientData;
+    [Header("Client Info")]
+    public string clientName;
+    public int age;
+    public string job;
+    public string city;
+    public string maritalStatus;
+    public string caseDescription;
+
+    [Header("Client financial info")]
+    public float monthlyIncome;
+    public float monthlyExpenses;
+    public float monthlySavings;
+    public float totalSavings;
+    public float neededLoan;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Client Name: " + clientData.clientName);
     }
 
     // Update is called once per frame
@@ -15,4 +33,5 @@ public class ClientTemplaet : MonoBehaviour
     {
         
     }
+
 }
---
+++ Assets/Scripts/Clients/JsonReader.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.IO;
+
+public class JsonReader : MonoBehaviour
+{
+    public string filePath;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        filePath = Application.dataPath + filePath;
+        string jsonContent = File.ReadAllText(filePath);
+
+        ClientData data = JsonUtility.FromJson<ClientData>(jsonContent);
+
+        //Debug.Log("Client Name: " + data.clientName);
+        //Debug.Log("Age: " + data.age);
+    }
+}
+
+[System.Serializable]
+public class ClientData
+{
+    public string clientName;
+    public int age;
+    public string job;
+    public string city;
+    public string maritalStatus;
+    public FinanceData Finance;
+    public string caseDescription;
+
+    public class FinanceData
+    {
+    }
+}
