987d74a2614d6972d28fc20078c86fc92c92a5e6
BingoMango <103103876+Dange1TheMange1@users.noreply.github.com>
2024-04-18 09:38:33 +0200

    the camera now behaves

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
+    Animation an;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        an = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("HHHHHHAAAAAAAAAAAAAAA");
+            an.Play("cameraswooper");
+        }
     }
 }
