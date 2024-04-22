7ee80ee02d2ac08c87994892db34c34cec62b012
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2024-04-22 16:07:27 +0200

    Assistant

---
+++ Assets/Scripts/Assistant.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assistant : MonoBehaviour
+{
+    public Animation walk;
+    public int dialoguToSay;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            assistantStartTalk();
+        }
+
+        if (Input.GetKeyDown("v"))
+        {
+            assistantNewLine();
+        }
+    }
+
+    public void assistantStartTalk()
+    {
+        walk.Play();
+        DialogueManager.instance.StartDia(dialoguToSay);
+    }
+
+    public void assistantNewLine()
+    {
+        DialogueManager.instance.nextSentince = dialoguToSay;
+    }
+}
