99315e72e912bab1acfd08d019b08d0c13ab824c
Alivia1620 <99955284+Alivia1620@users.noreply.github.com>
2024-04-30 14:52:06 +0200

    The scrollwheel works

--- Assets/Scripts/ScoreSiteScripts/CommentSystem.cs
+++ Assets/Scripts/ScoreSiteScripts/CommentSystem.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CommentSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject commentsInfo;
+    public Transform commentsParent;
+
+    public void PosComment()
     {
-        
+        GameObject newComment = Instantiate(commentsInfo, commentsParent);
+
+        TextMeshProUGUI commentText = newComment.GetComponentInChildren<TextMeshProUGUI>();
+
+        commentText.text = "Fuck du er dygtig";
+
+        //Debug.Log("Fuck du er dygtig");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NegComment()
     {
-        
+        GameObject newComment = Instantiate(commentsInfo, commentsParent);
+
+        TextMeshProUGUI commentText = newComment.GetComponentInChildren<TextMeshProUGUI>();
+
+        commentText.text = "Idioten er ikke god til at regne kursen ud";
+
+        //Debug.Log("Idioten er ikke god til at regne kursen ud");
     }
 }
