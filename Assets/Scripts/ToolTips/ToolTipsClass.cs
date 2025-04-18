8480c1ee77bbe8fe2aaf5312b2479553288bcc10
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2025-04-18 14:58:37 +0200

    VR body

---
+++ Assets/Scripts/ToolTips/ToolTipsClass.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipsClass
+{
+    public List<string> Terms;
+
+    public string Term_Titel;
+    public string Term_Body;
+}
